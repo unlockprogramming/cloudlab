c99734cc6433ec667a5d4f691b57acc47a6eccad
Bhuwan Prasad Upadhyay <bhuwan.upadhyay49@gmail.com>
2022-12-06 08:22:40 -0600

    fix: structure

--- docker-gogs-setup-and-init-repo-using-script/setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="api",
-    version="0.0.0",
-    packages=setuptools.find_packages(),
-    package_dir={"src": "src"},
-)
--- text-sentiment-scores-api/src/vader.py
+++
@@ -1,6 +0,0 @@
-class Vader:
-    def __init__(self):
-        pass
-
-    def score(self, text):
-        return {"score": 0.80, "label": "POSITIVE"}
