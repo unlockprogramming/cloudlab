ead5d9477f8524b5860d2f4fdb87e30d5c92d3c3
Bhuwan Prasad Upadhyay <40879634+bhuwanupadhyay@users.noreply.github.com>
2022-12-06 21:11:46 -0600

    Create pyenv_install.sh
    Signed-off-by: Bhuwan Prasad Upadhyay <40879634+bhuwanupadhyay@users.noreply.github.com>

---
+++ pyenv_install.sh
@@ -0,0 +1,45 @@
+
+required_version=$1
+if [ -z "$required_version" ]
+  then
+    echo "No 1st argument as required_version"
+    exit 1
+fi
+#!/bin/bash
+
+set -e
+
+required_version=$1
+
+pyenv_sh_rc_shell() {
+  rm -rf .pyenvshrc
+  echo 'export PYENV_ROOT="$HOME/.pyenv"' >>.pyenvshrc
+  echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >>.pyenvshrc
+  echo 'eval "$(pyenv init -)"' >>.pyenvshrc
+}
+
+pyenv_required_version() {
+    source .pyenvshrc
+    pyenv local "$required_version"
+    pyenv local "$required_version"
+    source .pyenvshrc    
+}
+
+pip_venv() {
+  python -m pip install --user virtualenv
+  python -m pip install --upgrade pip
+
+  virtualenv venv
+  source venv/bin/activate
+}
+
+if ! [ -x "$(command -v pyenv)" ]; then
+  echo 'Error: pyenv is not installed.' >&2
+  echo "source .pyenvshrc"
+  curl https://pyenv.run | bash
+fi
+
+pyenv_sh_rc_shell
+pyenv_required_version
+pip_venv
+pip install -r requirements.txt
