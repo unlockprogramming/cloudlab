6709d99a8560ea3470d474a12f04a5f978d65e4c
Bhuwan Prasad Upadhyay <40879634+bhuwanupadhyay@users.noreply.github.com>
2022-12-20 16:11:03 -0600

    Update get-docker-images-from-helm-releases.sh
    Signed-off-by: Bhuwan Prasad Upadhyay <40879634+bhuwanupadhyay@users.noreply.github.com>

--- bash/get-docker-images-from-helm-releases.sh
+++ bash/get-docker-images-from-helm-releases.sh
@@ -8,9 +8,11 @@ for row in $(helm list --all-namespaces -o json | jq -r '.[] | .name + ";" + .na
   release_name=$(echo "$row" | awk -F';' '{print $1}')
   namespace=$(echo "$row" | awk -F';' '{print $2}')
   chart=$(echo "$row" | awk -F';' '{print $3}')
+  echo ""
   echo "--------------------------------------------"
   echo "release => $chart | $release_name | $namespace"
   echo "--------------------------------------------"
   helm images get "$release_name" -n "$namespace" --from-release
   echo ""
+  
 done
