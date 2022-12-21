f1187c87a5ea609afa483c74e48d07c60c1af536
Bhuwan Prasad Upadhyay <bhuwan.upadhyay49@gmail.com>
2022-12-21 15:19:14 -0600

    fix: get docker images

--- bash/get-docker-images-from-helm-releases.sh
+++ bash/get-docker-images-from-helm-releases.sh
@@ -4,13 +4,30 @@ set -e
 
 helm plugin install https://github.com/nikhilsbhat/helm-images || true
 
-for row in $(helm list --all-namespaces -o json | jq -r '.[] | .name + ";" + .namespace + ";" + .chart'); do
-  release_name=$(echo "$row" | awk -F';' '{print $1}')
-  namespace=$(echo "$row" | awk -F';' '{print $2}')
-  chart=$(echo "$row" | awk -F';' '{print $3}')
-  echo ""
-  echo "-------------------------------------------------------------------------------------"
-  echo "release => $chart | $release_name | $namespace"
-  echo "-------------------------------------------------------------------------------------"
-  helm images get "$release_name" -n "$namespace" --from-release | sort | uniq
-done
+get_helm_charts_images() {
+
+  for row in $($1 | jq -r '.[] | .name + ";" + .namespace + ";" + .chart'); do
+    release_name=$(echo "$row" | awk -F';' '{print $1}')
+    namespace=$(echo "$row" | awk -F';' '{print $2}')
+    helm images get "$release_name" -n "$namespace" --from-release | sort | uniq >>"$log_file"
+  done
+
+}
+
+log_file=containers.log
+json_file=containers_json.log
+
+rm -rf "$log_file"
+rm -rf "$json_file"
+
+get_helm_charts_images "helm list --all-namespaces -o json"
+get_helm_charts_images "helm list --all-namespaces --pending -o json"
+
+echo "{" >>"$json_file"
+cat "$log_file" | sort | uniq | awk '{ print "\""$1"\"""," }' >>"$json_file"
+echo "\"nginx:latest\"" >>"$json_file"
+echo "}" >>"$json_file"
+cat "$json_file"
+
+rm -rf "$log_file"
+rm -rf "$json_file"
