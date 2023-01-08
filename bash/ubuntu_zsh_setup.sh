1ce29f05e154e8d34d1b683e87cdc73d7988594d
Bhuwan Prasad Upadhyay <40879634+bhuwanupadhyay@users.noreply.github.com>
2023-01-08 11:04:58 -0600

    Update ubuntu_zsh_setup.sh
    Signed-off-by: Bhuwan Prasad Upadhyay <40879634+bhuwanupadhyay@users.noreply.github.com>

--- bash/ubuntu_zsh_setup.sh
+++ bash/ubuntu_zsh_setup.sh
@@ -9,7 +9,3 @@ sudo apt install curl zsh git -y
 
 ## zsh ------------------------------------------------------
 sh -c "$(echo y | curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-echo -e "source \"\$HOME/.profile\"" | tee -a ~/.zshrc
