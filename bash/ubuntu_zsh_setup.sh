b33e695679f50ca1fb8e112fc3bac02234727cfa
Bhuwan Prasad Upadhyay <40879634+bhuwanupadhyay@users.noreply.github.com>
2023-01-08 11:13:58 -0600

    Update ubuntu_zsh_setup.sh
    Signed-off-by: Bhuwan Prasad Upadhyay <40879634+bhuwanupadhyay@users.noreply.github.com>

--- bash/ubuntu_zsh_setup.sh
+++ bash/ubuntu_zsh_setup.sh
@@ -5,11 +5,9 @@ set -e
 mkdir -p /tmp/tools
 cd /tmp/tools
 
-# curl & zsh ------------------------------------------------------
+## curl & zsh ------------------------------------------------------
 
-```bash
 sudo apt-get install -y curl zsh git
-```
 
 ## zsh ------------------------------------------------------
 sh -c "$(echo y | curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
