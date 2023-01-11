67e0a0b1051f8d1811c5e9cf1ddeb4ef0476c5d5
Bhuwan Prasad Upadhyay <40879634+bhuwanupadhyay@users.noreply.github.com>
2023-01-11 07:56:32 -0600

    Update ubuntu_arm_setup.sh
    Signed-off-by: Bhuwan Prasad Upadhyay <40879634+bhuwanupadhyay@users.noreply.github.com>

--- bash/ubuntu_arm_setup.sh
+++ bash/ubuntu_arm_setup.sh
@@ -93,10 +93,9 @@ sudo mv ./mc /usr/local/bin/
 ### END: common setup ------------------------------------------------------
 
 ### terraform setup ------------------------------------------------------
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=arm64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update -y && sudo apt install terraform -y
 terraform version
 
 ### jq setup ------------------------------------------------------
