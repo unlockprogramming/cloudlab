49ca73b365aae377ec2dfeebb8316de1913080ae
Bhuwan Prasad Upadhyay <40879634+bhuwanupadhyay@users.noreply.github.com>
2023-01-11 08:34:31 -0600

    Update and rename bash/arm64/ubuntu_setup2.sh to bash/arm64-ubuntu.sh
    Signed-off-by: Bhuwan Prasad Upadhyay <40879634+bhuwanupadhyay@users.noreply.github.com>

--- bash/arm64-ubuntu.sh
+++ bash/arm64-ubuntu.sh
@@ -1,21 +1,3 @@
-#!/bin/sh
-
-set -e
-
-mkdir -p /tmp/tools
-cd /tmp/tools
-
-if ! [ -x "$(command -v zsh)" ]; then
-
-## curl & zsh ------------------------------------------------------
-
-sudo apt-get install -y curl zsh git
-
-## zsh ------------------------------------------------------
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-fi
-
 #!/bin/bash
 
 set -e
